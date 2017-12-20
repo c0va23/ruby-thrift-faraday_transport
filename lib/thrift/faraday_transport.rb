0de616ddf4c3dcf3ba24007e02a428f196d21b0d
Dmitrij Fedorenko <c0va23@gmail.com>
2017-12-21 01:46:33 +0300

    Use YARD instead TomDoc

--- lib/thrift/faraday_transport.rb
+++ lib/thrift/faraday_transport.rb
@@ -9,14 +9,18 @@ module Thrift
     BASE_HEADERS = { 'Content-Type' => 'application/x-thrift' }.freeze
     DEFAULT_PATH = '/'.freeze
 
-    attr_reader :faraday_connection, :path
+    # @return [Faraday::Connection] attribute faraday_connection
+    attr_reader :faraday_connection
 
-    # Public: Initialize new FaradayTransport
+    # @return [String] attribute path
+    attr_reader :path
+
+    # Initialize new Faraday transport
     #
-    # faraday_connection - instance of Faraday::Connection
-    # path - optional endpoin path (Default: '/')
+    # @param faraday_connection [Faraday::Connection]
+    # @param path [String] optional endpoint (Default: '/')
     #
-    # Returns new Thrift::FaradayTransport
+    # @return [Thrift::FaradayTransport]
     def initialize(faraday_connection, path: DEFAULT_PATH)
       @faraday_connection = faraday_connection
       @path = path
