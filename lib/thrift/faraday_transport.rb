36f7a3902cd86ff87540b78b751dffab396748c6
Dmitrij Fedorenko <c0va23@gmail.com>
2017-12-20 10:52:44 +0300

    Add constructor for FaradayTransport

--- lib/thrift/faraday_transport.rb
+++ lib/thrift/faraday_transport.rb
@@ -1,7 +1,20 @@
 require 'thrift'
 
 module Thrift
+  # Public: Faraday transport for Thrift
   class FaradayTransport < Thrift::BaseTransport
     VERSION = Gem.loaded_specs['thrift-faraday_transport'].version.to_s
+
+    attr_reader :faraday_connection, :path
+
+    # Public: Initialize new FaradayTransport
+    #
+    # faraday_connection - instance of Faraday::Connection
+    #
+    # Returns new Thrift::FaradayTransport
+    def initialize(faraday_connection, path: nil)
+      @faraday_connection = faraday_connection
+      @path = path
+    end
   end
 end
--- spec/thrift/faraday_transport_spec.rb
+++ spec/thrift/faraday_transport_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'faraday'
 
 RSpec.describe Thrift::FaradayTransport do
   it 'has a version number' do
@@ -8,6 +9,49 @@ RSpec.describe Thrift::FaradayTransport do
   end
 
   it 'inherit Thrift::BaseTransport' do
-    is_expected.to be_a Thrift::BaseTransport
+    expect(described_class.superclass).to eq Thrift::BaseTransport
+  end
+
+  describe '#initialize' do
+    context 'without arguments' do
+      subject(:transport) { described_class.new }
+
+      it { expect { transport }.to raise_error(ArgumentError) }
+    end
+
+    context 'with faraday connection' do
+      subject(:transport) { described_class.new(faraday_connection) }
+
+      let(:faraday_connection) { Faraday.new }
+
+      it { expect { transport }.not_to raise_error }
+
+      it 'set faraday_connection' do
+        expect(transport.faraday_connection).to eq faraday_connection
+      end
+
+      it 'not set path' do
+        expect(transport.path).to be nil
+      end
+    end
+
+    context 'with faraday_connection and path' do
+      subject(:transport) do
+        described_class.new(faraday_connection, path: path)
+      end
+
+      let(:path) { 'custom prefix' }
+      let(:faraday_connection) { Faraday.new }
+
+      it { expect { transport }.not_to raise_error }
+
+      it 'set faraday_connection' do
+        expect(transport.faraday_connection).to eq faraday_connection
+      end
+
+      it 'not set path' do
+        expect(transport.path).to be path
+      end
+    end
   end
 end
