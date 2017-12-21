e579c581c80674005f65fde751b860250d219685
Dmitrij Fedorenko <c0va23@gmail.com>
2017-12-21 10:28:57 +0300

    Raise error on not 200 status

--- lib/thrift/faraday_transport.rb
+++ lib/thrift/faraday_transport.rb
@@ -8,6 +8,14 @@ module Thrift
     VERSION = Gem.loaded_specs['thrift-faraday_transport'].version.to_s
     BASE_HEADERS = { 'Content-Type' => 'application/x-thrift' }.freeze
 
+    # Unexpected HTTP code raise #flush when HTTP respond with not 200 status
+    class UnexpectedHTTPCode < TransportException
+      def initialize(http_code)
+        super(TransportException::UNKNOWN, "Invalid HTTP code #{http_code}")
+        @http_code = http_code
+      end
+    end
+
     # @return [Faraday::Connection] attribute faraday_connection
     attr_reader :faraday_connection
 
@@ -33,11 +41,15 @@ module Thrift
       @in_buffer.read(size)
     end
 
+    # Perform HTTP request. Implement Thrift::BaseTransport#flush
+    #
+    # @raise [UnexpectedHTTPCode] when HTTP server respond with not 200 status
     def flush
       response = @faraday_connection.post do |request|
         request.body = @out_buffer
         request.headers.merge!(BASE_HEADERS)
       end
+      raise UnexpectedHTTPCode, response.status if response.status != 200
       body = Bytes.force_binary_encoding(response.body)
       @in_buffer = StringIO.new(body)
     ensure
--- spec/thrift/faraday_transport_spec.rb
+++ spec/thrift/faraday_transport_spec.rb
@@ -89,6 +89,23 @@ RSpec.describe Thrift::FaradayTransport do
       end
     end
 
+    context 'with transport url have invalidpath' do
+      let(:path) { '/invalidpath' }
+      let(:url) { super().merge(path) }
+
+      before 'prepare flush' do
+        transport.write(request_body)
+        faraday_stabs.post(path, request_body, headers) do |_env|
+          [404, described_class::BASE_HEADERS, 'Not found']
+        end
+      end
+
+      it 'allow read valid response body' do
+        expect { flush }.to raise_error(described_class::UnexpectedHTTPCode,
+                                        /404/)
+      end
+    end
+
     context 'when flush called not first time' do
       let(:other_request_body) { SecureRandom.random_bytes(20) }
       let(:other_response_body) { SecureRandom.random_bytes(40) }
